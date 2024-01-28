d4710d061183012982fc3ac177208fe232a62e67
prosp3ro <g0lden_vo1d@protonmail.com>
2024-01-28 18:34:02 +0100

    feat: add jwt authentication to models

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -67,4 +67,24 @@ class Admin extends Model
     // protected $casts = [
     //     'password' => 'hashed',
     // ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
