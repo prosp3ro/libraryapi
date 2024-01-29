b3900c892122ad1c5bd5781cd3cb484f1b3f6cbd
prosp3ro <g0lden_vo1d@protonmail.com>
2024-01-29 11:26:36 +0100

    feat: create database factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Admin::factory(10)->create();
+        Book::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
